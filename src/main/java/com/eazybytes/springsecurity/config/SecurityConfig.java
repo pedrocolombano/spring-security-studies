c973fe211703eea786dfb0731cd4c49c49260fc4
Pedro Colombano <pedromaiacolombano@gmail.com>
2025-01-29 00:26:04 -0300

    feat: add default users and password encoder type

--- src/main/java/com/eazybytes/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/eazybytes/springsecurity/config/SecurityConfig.java
@@ -2,7 +2,14 @@ package com.eazybytes.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,7 +22,29 @@ public class SecurityConfig {
             req.anyRequest().authenticated();
         });
 
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                               .password("{noop}12345")
+                               .authorities("read")
+                               .build();
+
+        UserDetails admin = User.withUsername("admin")
+                                .password("{bcrypt}$2a$12$0Ytn3WRjz6G/2Hg8q1BDQ.gpJ89rA0RU8V/PtQaftta01sUHLVBlK")
+                                .authorities("admin")
+                                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
